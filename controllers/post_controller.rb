779de4dc4ddba222c50b317c68453dd428f0a390
Chinenye Anyadi <canyadi@spartaglobal.com>
2018-10-25 20:48:06 +0100

    links worl but to the next page

--- controllers/post_controller.rb
+++ controllers/post_controller.rb
@@ -1,58 +1,57 @@
-# class is the child of sinatra base
-class PostController < Sinatra::Base
-# for rack to work
-  set :root, File.join(File.dirname(__FILE__), "..")
-  #
-  set :view, Proc.new { File.join(root, "Views") }
 
-  configure:development do
-    register Sinatra::Reloader
-  end
-# creating the Hash.new
+# this class will be a child of sinatra base
+class PostController < Sinatra::Base
+# for rack to work!!!!!!!!!!! its's a must
+set :root, File.join(File.dirname(__FILE__), "..")
+#
+set :view, Proc.new { File.join(root, "Views") }
+
+configure:development do
+  register Sinatra::Reloader
+end
+# find out the meaning of dollar sign in ruby
 $videos = [
-  {
-    id: 1,
-    title: "Video 1",
-    description:"This video is about ........."
+
+  {id:1,
+    title: "Video-1",
+    body: "esbrjfhjb."
   },
-  {
-    id: 2,
-    title: "Video 2",
-    description:"This video is about ........."
-  },
-  {
-    id:3,
-    title:"Video 3",
-    description: "This video is about....."
-  },
-  {
-    id:4,
-    title:"Video 4",
-    description:"This video is about...."
-  },
-  {
-    id:5,
-    title:"Video 5",
-    description:"This video is about........."
-  }
+{ id:2,
+  title: "Video-2",
+  body: "ekrhbjbfjsf"
+},
+{ id:3,
+  title: "Video-3",
+  body: "ekrhbjbfjsf"
+},
+{ id:4,
+  title: "Video-4",
+  body: "ekrhbjbfjsf"
+},
+{ id:5,
+  title: "Video-5",
+  body: "ekrhbjbfjsf"
+}
 ]
-  # static dynamic
+  # static route- doesn't change
   get "/" do
-  @title_homepage = "Index 1"
-  @videos = $videos
-  erb :'posts/index'
+    # "<h1>hello Sinatra</h1>"
+    @title_for_the_page = "Blog Posts"
+    # to get all the values for posts
+    @videos = $videos
+    erb :'posts/index'
+
   end
 
   get "/new" do
-    "<h1> hi Lorem ipsum t anim id est laborum.</h1>"
+    "<h1>hello Sinatra new page</h1>"
   end
 
-  get "/:id_from_video" do
-    id = params[:id_from_video].to_i
-    @post = $videos[id]
+  # dynamic route- changes
+  get "/:id_from_URL" do
+    id = params[:id_from_URL].to_i
+    @videos = $videos[id]
     erb :'posts/video1'
   end
-
-
 
 end
