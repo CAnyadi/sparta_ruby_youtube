ec73877fd46329774160e701181cc5121fb4d530
Chinenye Anyadi <canyadi@spartaglobal.com>
2018-10-25 15:58:12 +0100

    the /new code in post controller

--- controllers/post_controller.rb
+++ controllers/post_controller.rb
@@ -9,13 +9,46 @@ class PostController <Sinatra::Base
     register Sinatra::Reloader
 
   end
+# creating the Hash.new
+$videos = [
+  {
+    id: 1,
+    title: "Video 1",
+    description:"This video is about ........."
+  },
+  {
+    id: 2,
+    title: "Video 2",
+    description:"This video is about ........."
+  },
+  {
+    id:3,
+    title:"Video 3",
+    description: "This video is about....."
+  },
 
+  {
+    id:4,
+    title:"Video 4",
+    description:"This video is about...."
+  },
+  {
+    id:5,
+    title:"Video 5",
+    description:"This video is about........."
+  }
+]
   # static dynamic
+  get "/" do
+  @title_homepage = "Index 1"
+  @videos = $videos
+  erb :'posts/index'
+  end
 
-get "/" do
+  get "/new" do
+    "<h1> hi Lorem ipsum t anim id est laborum.</h1>"
+  end
 
-@title_homepage = "Index 1"
-erb :'posts/index'
-end
+
 
 end
