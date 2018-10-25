a0c5501a4c30b7f607d72a03166a05f31a025a45
Chinenye Anyadi <canyadi@spartaglobal.com>
2018-10-25 15:27:22 +0100

    put 6 erbs and set up the post controller

---
+++ controllers/post_controller.rb
@@ -0,0 +1,17 @@
+# class is the child of sinatra base
+class PostController <Sinatra::Base
+# for rack to work
+  set :root, File.join(File.dirname(__FILE__), "..")
+  #
+  set :view, Proc.new { File.join(root, "Views") }
+
+  configure:development do
+    register Sinatra::Reloader
+
+  end
+
+  # static dynamic
+
+get "/" do
+
+@title_homepage = "Index 1"
