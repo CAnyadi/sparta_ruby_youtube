dc22280c7d4d641c0d7511c93be753b30fb6e295
Chinenye Anyadi <canyadi@spartaglobal.com>
2018-10-27 22:02:28 +0100

    added videos,footer, logo and description

--- controllers/post_controller.rb
+++ controllers/post_controller.rb
@@ -13,35 +13,49 @@ end
 $videos = [
 
   {id:0,
-    title: "Video-1",
-    body: "esbrjfhjb.",
+    title: "GRWM",
+    body: "Make sure you click 1080 HD.
+---------------------------------------------------------------------------------------
+If you guys want to connect with me you can follow me on
+INSTAGRAM. melachild
+TWITTER. melachild
+SNAPCHAT. naomibankss
+CONTACT EMAIL : melachild@hotmail.com
+---------------------------------------------------------------------------------
+
+",
     link: "z1LELyptiDI"
   },
 { id:1,
-  title: "Video-2",
-  body: "ekrhbjbfjsf",
+  title: "GirlTalk",
+  body: "Hey dolls, will fill in later",
   link: "-3CFKAGPdtc"
 },
 { id:2,
-  title: "Video-3",
-  body: "ekrhbjbfjsf",
+  title: "Hair-Review",
+  body: "LIKE AND SUSCRIBE HUNNIES
+---------------------------------------------------------------------------------------
+IF I TELL YOU WHAT HAS HAPPENED TO ME THIS WEEK LMAO, IT'S ONLY GOD!
+
+I'm so sorry for the late upload a lot has been going on but anyway, I needed to let you guys know that you need to stop messing with these trash vendors and get with peerless hair. I mean look at what their hair has done to me !!! It's got me moving mad different I dont even breathe the same air as everyone.",
   link: "toIoRnaDdrU"
 },
 { id:3,
-  title: "Video-4",
-  body: "ekrhbjbfjsf",
+  title: "Talk-time",
+  body: "Hello my lovelies
+, It's been a minute but here it is ",
   link: "WCppH6Jvn5Q"
 },
 { id:4,
-  title: "Video-5",
-  body: "ekrhbjbfjsf",
+  title: "Get to know me",
+  body: "It has been a minute I know I had to get my mind right, I wouldnt say I've fully recovered but I'm doing better as each day goes by, I hope you can find it in your heart to forgive me for being inconsiderate and not thinking about you all and putting my needs first. I've learnt alot these past few months and the experiences I've encountered will definetly shape me for the rest of my life, right now I'm just coming to terms with it and trying to learn as much as I can. Experience is the best teacher I'm only realising  that now. Well I hope you guys enjoy the video ",
   link: "vTke0eK0cUY"
 }
 ]
   # static route- doesn't change
   get "/" do
     # "<h1>hello Sinatra</h1>"
-    @title_for_the_page = "Blog Posts"
+    @title_for_the_page = "Homepage"
     # to get all the values for posts
     @videos = $videos
     erb :'posts/index'
